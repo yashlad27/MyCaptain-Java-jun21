0a6790059e5663312697701da98ec5b2d10a4938
yashlad27 <71868797+yashlad27@users.noreply.github.com>
2021-06-08 19:08:18 +0530

    Add files via upload

---
+++ myCaptain_task1.java
@@ -0,0 +1,10 @@
+package java_home;
+
+public class myCaptain_task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
