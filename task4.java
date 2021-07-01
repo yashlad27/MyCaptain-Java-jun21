17e57470d0184694f103935ee8d2b077bcc81f87
yashlad27 <71868797+yashlad27@users.noreply.github.com>
2021-07-01 20:39:25 +0530

    Add files via upload

---
+++ task4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class task4 {
+
+    public static void main(String[] args) {
+
+        int choice;
+        System.out.println("Pick one: 1.Hi\t 2.Hey\t 3.hello\t");
+        Scanner s = new Scanner(System.in);
+        choice = s.nextInt();
+        switch (choice) {
+            case 1 -> System.out.println("You said Hi");
+            case 2 -> System.out.println("You said Hey");
+            case 3 -> System.out.println("You said hello");
+            default -> System.out.println("Select one option");
+        }
+    }
+}
